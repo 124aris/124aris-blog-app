71ad922e7182f875b35364fdd82cff13acf04c60
Muhammad Haris <mharis01470017@gmail.com>
2024-01-20 23:08:18 +0500

    blog-app

---
+++ src/app/blogs/[id]/page.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const getBlogData = async (id: number) => {
+  const res = await fetch(
+    `https://api.slingacademy.com/v1/sample-data/blog-posts/${id}`
+  );
+  if (!res.ok) {
+    throw new Error("Could Not Retrieve Blog Posts");
+  }
+  return await res.json();
+};
+export default async function BlogDetail({ params }: any) {
+  const { blog } = await getBlogData(params.id);
+
+  return (
+    <div className="p-24 flex flex-col gap-1 flex-wrap">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        {blog.title}
+      </h1>
+      <p>{blog.description}</p>
+      <p>{`${blog.content_text}`}</p>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,10 +22,34 @@ export default async function Home() {
   const Blogs = await getBlogs();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="p-10 flex flex-col justify-center items-center">
       {Blogs.blogs.length > 0 ? ( 
-        <div>
-          Blogs
+        <div className="flex gap-5 flex-wrap">
+          {Blogs.blogs.map((post:any) => (
+            <Link href={`/blogs/${post.id}`} key={post.id}>
+              <Card>
+                <CardContent className="max-w-xs flex flex-col gap-1 p-4">
+                  <Image
+                    src={post.photo_url}
+                    alt={post.title}
+                    width={400}
+                    height={400}
+                    className="object-cover"
+                  />
+                  <CardTitle>{post.title}</CardTitle>
+                  <CardDescription>{post.description}</CardDescription>
+                  <div className="flex justify-between items-center">
+                    <Badge variant="default" className="capitalize">
+                      {post.category}
+                    </Badge>
+                    <p className="font-bold">
+                      Posted:{dayjs(post.created_at).format("DD-MMM-YY")}
+                    </p>
+                  </div>
+                </CardContent>
+              </Card>
+            </Link>
+          ))}
         </div> 
       ):( <div>No Blog Found</div> )}
     </main>
